749f936254452b2067a24a6892f02b56c470cb68
HarryHoudini <dev.jon.jonson@gmail.com>
2020-12-20 15:32:33 +0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Article from './Article'
+import fixtures from '../fixtures'
+
+function App() {
+    return <div>
+      <h1>App name</h1>
+      <Article article={fixtures[0]} />
+    </div>;
+}
+
+export default App;
