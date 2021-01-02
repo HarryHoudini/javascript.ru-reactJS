199b53a9f0047eff23a2d21fe776d15658cb5b0c
HarryHoudini <dev.jon.jonson@gmail.com>
2021-01-02 12:56:57 +0300

    Add style.css for article

--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import Article from "./Article";
+import Article from "../Article";
+import './style.css'
+
 export default function ArticleList({ articles }) {
     const articlesElements = articles.map((article) => (
-        <li key={article.id}>
+        <li key={article.id} className='article-list__li'>
             <Article article={article} />
         </li>
     ));
