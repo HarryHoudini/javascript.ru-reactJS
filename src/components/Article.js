933bcee3bcca03bc963a526761e4a380cae29fa9
HarryHoudini <dev.jon.jonson@gmail.com>
2021-01-02 11:15:43 +0300

    Add state to article component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Article(props) {
-    const { article } = props;
-    console.log('----', props)
-    const body = <section>{article.text}</section>;
-    return (
-        <div>
-            <h2>{article.title}</h2>
-            {body}
-            <h3>creation date: {new Date(article.data).toDateString()}</h3>
-        </div>
-    );
+class Article extends Component {
+    state = {
+        isOpen: false,
+    };
+    render() {
+        const { article } = this.props;
+        console.log("----", this.props);
+        const body = this.state.isOpen && <section>{article.text}</section>;
+        return (
+            <div>
+                <h2>{article.title}</h2>
+                <button onClick={handleClick}>close</button>
+                {body}
+                <h3>creation date: {new Date(article.date).toDateString()}</h3>
+            </div>
+        );
+    }
 }
 
+function handleClick() {
+    console.log("----", "clicked");
+}
 export default Article;
